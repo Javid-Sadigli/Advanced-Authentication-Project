92363184276bfd8dda519d42c435e39003477a56
Javid Sadigli <cavidoffical@gmail.com>
2024-01-29 23:58:06 +0400

    Finished password reset form page

--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ router.get('/signup', auth_controller.GET_Sign_Up);
 router.get('/signin', auth_controller.GET_Sign_In);
 router.get('/verify_info', auth_controller.GET_Verify_Info);
 router.get('/verified_info', auth_controller.GET_Verified_Info);
+router.get('/password_reset_form', auth_controller.GET_Password_Reset_Form);
 
 router.post('/signup', auth_controller.POST_Sign_Up);
 router.post('/signin', auth_controller.POST_Sign_In);
